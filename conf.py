1bb81a4058de9d122c418ba52e0a160c9cc49f7f
AlanBommer <60212768+AlanBommer@users.noreply.github.com>
2020-02-03 11:06:21 +0100

    Add files via upload

--- conf.py
+++ conf.py
@@ -10,16 +10,16 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
 
-project = 'blocsSmarthPath'
+project = 'Projet Industriel : SmartCrawl'
 copyright = '2020, Alan'
-author = 'Alan'
+author = 'Jérémie FABRE, Alan JOUBIOUX, Nicolas SCAËROU'
 
 
 # -- General configuration ---------------------------------------------------
@@ -27,7 +27,8 @@ author = 'Alan'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['recommonmark']
+extensions = [
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
